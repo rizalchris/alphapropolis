d4e8b056046435b584c66e5c129f8d38b270b36c
rizalchris <rizalchrisdev@gmail.com>
2026-06-27 15:33:49 +0700

    feat: optimize SEO, improve UI components, and update product pricing packages with new image assets

---
+++ src/lib/site.ts
@@ -0,0 +1,2 @@
+export const whatsappOrderUrl =
+	"https://api.whatsapp.com/send?phone=628118131000&text=Hallo%20Mbak%20Alphi%2C%E2%98%BA%EF%B8%8F%0ASaya%20mau%20order%20Alpha%20Propolisnya%20dong%2C%0ANama%3A%0AAlamat%20Lengkap%3A%0AJumlah%20Botol%3A";
